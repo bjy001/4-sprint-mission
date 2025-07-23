39735d8afc50754d821d1dcb36926b8270d405ef
JaeYun Byun <bjy001@naver.com>
2025-07-23 09:35:16 +0900

    Product추가

---
+++ ElectronicProduct.js
@@ -0,0 +1,9 @@
+class ElectronicProduct extends Product {
+    
+    constructor(manufacturer){
+        super(name, description, price, tags, images, favoriteCount);
+        this.manufacturer = manufacturer;
+    }
+    
+
+}
