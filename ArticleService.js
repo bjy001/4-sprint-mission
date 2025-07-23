fc764f3e79d2095e5523267e43159e874a6d191d
JaeYun Byun <bjy001@naver.com>
2025-07-23 16:31:39 +0900

    Axios 메소드추가

--- ArticleService.js
+++ ArticleService.js
@@ -5,14 +5,14 @@
  * */
 import axios from 'axios';
 
-// #[Get Request 예제1]
+// #[Get Request 보내기 예제1]
 // 설문객체 목록
 // Request 보낼때 쿼리 스트링 추가 가능: mbti limit, offset
 export async function getColorSurveyList(){
   //URL객체 사용
   const url = new URL('https://learn.codeit.kr/api/color-surveys'); 
   //URL객체의 searchParams 라는 프로퍼티에 추가
-  url.searchParams.append('offset', 1);
+  url.searchParams.append('offset', 0);
   url.searchParams.append('limit', 5);
   url.searchParams.append('mbti', 'ISTJ');
 
@@ -21,7 +21,7 @@ export async function getColorSurveyList(){
   return data;
 }
 
-// #[Get Request 예제2]
+// #[Get Request 보내기 예제2]
 // 설문객체 1개
 export async function getColorSurvey(id){
   const url = new URL(`https://learn.codeit.kr/api/color-surveys/${id}`);
@@ -31,6 +31,131 @@ export async function getColorSurvey(id){
   return data;
 }
 
+// #[Post Request 보내기 예제1]
+/**
+ *  fetch 함수는 기본적으로 GET Request를 보냄
+ *  - POST나 다른 종류의 Request를 보낼때 사용 .. 두번째 아규먼트에 method를 POST 로 설정하면 됨
+ *    POST 뿐만 아니라 PATCH, DELETE도 사용
+ *  - POST Request는 보통 body를 같이 전달함.
+ * 
+ *  survey 객체를 만들고 그것을 POST Request의 body로 전달해서 설문결과를 생성
+ */
+  export async function createColorSurvey(surveyData){
+    console.log('====> (API) createColorSurvey..');
+    // survey 데이터는 자바스크립트 객체이고 외부로 문자를 주고 받을때는 JSON문자열을 사용한다.
+    // 그래서 survey 문자열을 JSON 문자열로 변환해 주어야 한다.
+    // const surveyData = {
+    //   mbti: 'ISTJ',
+    //   colorCode: '#110000',
+    //   password: '0000'
+    // };
+    //Request 롤 아래와 같이 옵션으로 설정한다.
+    const res =  await fetch('https://learn.codeit.kr/api/color-surveys', {
+                  method: 'POST',
+                  //survey 데이터는 자바스크립트 객체이고 외부로 문자를 주고 받을때는 JSON문자열을 사용한다.
+                  body: JSON.stringify(surveyData), 
+                  //데이터 보낼때 어떤 형식의 데이터를 보낼 것인지 알려주는게 좋다. ==> headers프로퍼티로 헤더 설정
+                  //필요하다면 다른 헤더도 아래에 추가
+                  headers: {
+                    'Content-Type': 'application/json'
+                  }
+              });
+
+     if (!res.ok) {
+        throw new Error('데이터를 생성하는데 실패했습니다.'); //응답Response가 제대로 안올때... 200 이 true
+     };
+
+    //Post Request로 데이터를 생성하면 서버가 생성된 데이터를 Response 로 돌려주는 경우가 많은데
+    //설문생성 결과가 Response로 돌아오면 json() 메소드로 Response를 파싱해서 출력.          
+    const data = await res.json();
+
+    //새로 생성된 설문 객체가 응답된 것을 볼 수 있다
+    return data;
+ };
+
+// #[Fetch함수 사용시 오류처리]
+/**
+ *  Fetch 오류는 2가지 형태 .. 1.URL이 이상하거나 헤더정보가 이상해서 Request 자체가 실패하는 경우
+ *                               ==> try..catch 문을 이용한다 (이 케이스만 Promise 상태가 REJECT 가 됨)
+ *   2. Request는 성공적인데 상태코드가 실패를 나타내는 경우
+ *                               ==> 또는 응답 Response 결과를 보고 Error를 리턴한다. (수동적으로 오류를 throw함)
+ *  ==> Response가 성공적인 결과가 돌아왔을때만 데이터를 리턴하고 나머지 모든 경우에는 오류를 발생
+ * 
+ * */
+export async function getColorSurveys(params = {}){
+  const url = new URL('https://learn.codeit.krrr/api/color-surveys');
+
+  const res = await fetch(url);
+
+/**
+  * 400, 500 Response 에러는 Fetch Promise는 fulfilled 상태가 됨
+  *   ==> 깔끔하게 처리하는 방법은 Response의 상태가 성공을 나타내지 않으면 오류를 발생시킴
+  * 예를 들면 잘못된 mbti 파라미터를 전달하면 상태코드 400이 돌아옴. 상태코드 400이 돌아와도
+  * 아무문제 없이 res.json()이 실행됨.  ==> 그대로 응답body가 리턴됨(함수안에서 아예 오류가 발생안함)
+  */
+  if(!res.ok){ //Response 의 상태코드가 2로 시작하면 true 아니먄 false
+    throw new Error('데이터를 불러오는데 실패했습니다.');
+  }
+  
+  const data = await res.json();
+  return data;
+}
+
+// #[Axios Get Request 예제1]
+/**
+ *  - Fetch보다 문법이 간단하고 다양한 편의 기능을 제공하기 때문에 실무에서 많이 사용
+ *  - 3rd Party 모듈이기 때문에 설치 필요.(터미널)
+ *     : npm install axios
+ *  
+ */
+export async function getAxiosColorSurvey(id){
+  console.log('====> (API) getAxiosColorSurvey..');
+  const url = new URL(`https://learn.codeit.kr/api/color-surveys/${id}`);
+  /**
+   * Axios로 Get Request를 보내려면 axios.get 메소드를 사용
+   * Axios도 Promise를 리턴하기 때문에 await 사용
+   */
+  const res = await axios.get(url);
+  // Axios 에서는 body내용을 파실할 필요 없이 그냥 data 프로퍼티로 가져오면 됨.
+  // data변수에 할당 할 필요없이 그대로 리턴
+  return res.data;
+}
+
+// #[Axios Get Request 예제2]
+/**
+ *  - 쿼리 파라미터도 좀더 쉽게 처리가능한데 URL객체를 사용할 필요 없이 
+ *    params 옵션을 사용하면 됨.
+ */
+ export async function getAxiosColorSurveys(params = {}){
+  console.log('====> (API) getAxiosColorSurveys..');
+  /**
+   * params 라는 프로퍼티로 params라고 객체를 설정하는것
+   *  : 이렇게 쿼리 파라미터를 담고있는 객체를 전달하면 객체에 있는 프로퍼티별로 
+   *    알아서 쿼리스트링을 만들고 URL뒤에 붙여서 Request를 보내줌.
+   *    만약 프로퍼티값이 null이나 undefined 이면 그 프로퍼티를 무시하고
+   *    쿼리스트링을 만들어 줌.
+   */
+  const res = await axios.get('https://learn.codeit.kr/api/color-surveys', 
+      { params }
+    );
+  return res.data;
+ }
+
+ // #[Axios Post Request 예제1]
+ /**
+  *  - Get Request는 axios.get 을 사용하고 Post Request는 axios.post 를 사용함
+  *  - Post 메소드는 body로 전달할 데이터를 두번째 Argument로 받는데 자바스크립트 객체를
+  *    그대로 사용할 수 있다. ==> axios 가 알아서 자바스크립트 객체를 JSON 문자열로 변환해 준다.
+  *  - 헤더도 body데이터를 보고 알아서 설정하기 때문에 우리가 설정해줄 필요가 없다.
+  */
+  export async function createAxiosColorSurvey(surveyData) {
+    const res = await axios.post('https://learn.codeit.kr/api/color-surveys',
+      surveyData  //body 데이터를 2번째 아규먼트로 받는다
+    );
+    return res.data;
+  };
+
+  
 
 //Article 목록
 export async function getArticleList(page = 1, pageSize = 5, keyword = ""){
--- main.js
+++ main.js
@@ -5,20 +5,46 @@
  *
  */
 
-// import { fetchSampe, getArticle, getBadArticle, createArticle, getArticleList, deleteArticle, 
-//          getAxiosArticle, getAxiosArticleList, createAxiosArticle, deleteAxiosArticle } from './article/ArticleService.js';
-// import { getProduct, getProductList, createProduct, deleteProduct, patchProduct,
-//          getAxiosProduct, getAxiosProductList, createAxiosProduct, deleteAxiosProduct } from './product/ProductService.js';
-
-import { getColorSurveyList, getColorSurvey, getArticleList, getArticle, getAxisArticle } from './ArticleService.js'
+import { getColorSurveyList, getColorSurvey, createColorSurvey, getColorSurveys, getAxiosColorSurvey, 
+         getAxiosColorSurveys,  createAxiosColorSurvey, 
+         getArticleList, getArticle, getAxisArticle } from './ArticleService.js'
 
 async function main(){
     /* #[Get Request 예제1] */
-    // const colorDataList = await getColorSurvey();
+    // const colorDataList = await getColorSurveyList();
     // console.log(colorDataList);
     /* #[Get Request 예제2] */
-    const colorData = await getColorSurvey(52);
-    console.log(colorData);
+    // const colorData = await getColorSurvey(52);
+    // console.log(colorData);
+
+    /* #[Post Request 예제1] */
+    const surveyData = {
+      mbti: 'ISTJ',
+      colorCode: '#110000',
+      password: '0000'
+    };
+    // const resData = await createColorSurvey(surveyData);
+    // console.log(resData);
+
+    /* #[Fetch함수 사용시 오류처리] */
+    // try {
+    //  const resErrData = await getColorSurveys();
+    //  console.log(resErrData);
+    // } catch (e) {
+    //   console.log('오류가 발생했습니다.');
+    //   console.log(e.message); //Promise 객체가 rejected 상태
+    // }
+
+    /* #[Axios Get Request 예제1] */
+    // const axiosData = await getAxiosColorSurvey(52);
+    // console.log(axiosData);
+    /* #[Axios Get Request 예제2] */
+    // const axiosList = await getAxiosColorSurveys();
+    // console.log(axiosList);
+    /* #[Axios Post Request 예제1] */
+    // const rstAxiosData = await createAxiosColorSurvey(surveyData);
+    // console.log(rstAxiosData);
+
 
     // ############### [1] Article ########################
     // const articleList = await getArticleList();
